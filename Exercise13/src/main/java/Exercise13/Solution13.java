74e95c04da6e25bda8a257f9adc86aa8a48a34c7
Srignan <Srignan7@gmail.com>
2021-09-18 01:01:57 -0400

    13 Complete

--- Exercise13/src/main/java/Exercise13/Solution13.java
+++ Exercise13/src/main/java/Exercise13/Solution13.java
@@ -6,10 +6,34 @@
 package Exercise13;
 
 /*
-User prompted inputs for three items quantity and price
-Use string format again to make the output stop at 2 decimal places
-Calculate outputs using multiplication and addition
+Scan all user inputs
+Import math lang so that I can use math pow for exponents
+Add formula of compound interest and use math ceil to round up
  */
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Solution13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the principal amount? ");
+        int Principal = input.nextInt();
+
+        System.out.print("What is the rate? ");
+        double Rate = input.nextDouble();
+
+        System.out.print("What is the number of years? ");
+        int Year = input.nextInt();
+
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        int n = input.nextInt();
+
+        double Final = Principal*Math.pow(1+((Rate/100)/n), n * Year);
+        double TrueFinal = Math.ceil(Final*100.00)/100.00;
+
+
+        System.out.printf("$%d invested at %.1f%% for %d years compounded %d times per year is $%.2f.\n",
+                Principal, Rate, Year, n, TrueFinal);
+    }
+}
