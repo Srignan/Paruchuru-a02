4333237183de76b86bbd7160eead34f7ec08c211
Srignan <Srignan7@gmail.com>
2021-09-19 16:17:04 -0400

    21 Complete

--- Exercise21/src/main/java/Exercise21/Solution21.java
+++ Exercise21/src/main/java/Exercise21/Solution21.java
@@ -1,13 +1,45 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 20 Solution
+ *  UCF COP3330 Fall 2021 Assignment 21 Solution
  *  Copyright 2021 Srignan Paruchuru
  */
 
 package Exercise21;
 
+import java.util.Scanner;
+
 /*
-Use ternary operator for print statements; if age >=16, true = old enough to drive, false = not old enough
-Scan user input for age
+Switch statements for each month, convert int to month
  */
 public class Solution21 {
+
+    public static String getMonth(int month){
+        return switch (month) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "error";
+        };
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the number of month: ");
+        int month = input.nextInt();
+
+        if(month >= 1 && month <= 12){
+            System.out.println("The name of month is "+getMonth(month));
+        } else {
+            System.out.println(getMonth(month)+": Invalid month");
+        }
+    }
 }
