fdb5db11c03eb5f9b3d5b46c5ad1c398db3ad01f
Srignan <Srignan7@gmail.com>
2021-09-19 11:06:25 -0400

    18 Complete

--- Exercise18/src/main/java/Exercise18/Solution18.java
+++ Exercise18/src/main/java/Exercise18/Solution18.java
@@ -6,9 +6,35 @@
 package Exercise18;
 
 /*
-Use ternary operator for print statements; if age >=16, true = old enough to drive, false = not old enough
-Scan user input for age
+Scan letter choice, give different output if user inputs something other than F or C.
+Plug in formula for Fahrenheit to Celsius and vice versa in appropriate parts of the if else statement
+Use .toUpperCase so that the program reads lower case
  */
 
-public class Solution18 {
+import java.util.Scanner;
+
+public class Solution18{
+        public static void main(String[] args) {
+            Scanner Input = new Scanner(System.in);
+            System.out.println("Press C to convert from Fahrenheit to Celsius.");
+            System.out.println("Press F to convert from Celsius to Fahrenheit.");
+            System.out.print("Your choice: ");
+            char Choice = Input.next().toUpperCase().charAt(0);
+            int Temperature, Conversion;
+
+
+            if (Choice == 'C') {
+                System.out.print("Please enter the temperature in Fahrenheit: ");
+                Temperature = Input.nextInt();
+                Conversion = (Temperature - 32) * 5 / 9;
+                System.out.print("The temperature in Celsius is " + Conversion + ".");
+            } else if (Choice == 'F') {
+                System.out.print("Please enter the temperature in Celsius: ");
+                Temperature = Input.nextInt();
+                Conversion = (Temperature * 9 / 5) + 32;
+                System.out.print("The temperature in Fahrenheit is " + Conversion + ".");
+            } else {
+                System.out.println("Illegal Input");
+            }
+        }
 }
