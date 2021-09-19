e8c6961d8ab6a8d9e2411298130c8296d6afde03
Srignan <Srignan7@gmail.com>
2021-09-19 16:36:34 -0400

    22 Complete

--- Exercise22/src/main/java/Exercise22/Solution22.java
+++ Exercise22/src/main/java/Exercise22/Solution22.java
@@ -1,13 +1,50 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 20 Solution
+ *  UCF COP3330 Fall 2021 Assignment 22 Solution
  *  Copyright 2021 Srignan Paruchuru
  */
 
 package Exercise22;
 
+import java.util.Scanner;
+
 /*
-Use ternary operator for print statements; if age >=16, true = old enough to drive, false = not old enough
-Scan user input for age
+Scan for three different numbers from user
+If statements for num1 is larger than 2 and 3, and for if 2 is larger than 3
+Return if all numbers are equal
  */
+
 public class Solution22 {
+    public static void main(String[] args)
+    {
+        int Num1, Num2, Num3;
+        Scanner Input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        Num1 = Input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        Num2 = Input.nextInt();
+
+        System.out.print("Enter the third number: ");
+        Num3 = Input.nextInt();
+
+        if(Num1==Num2 && Num1 == Num3){
+            System.exit(0);
+        }
+
+        else if(Num1 >= Num2 && Num1 >= Num3)
+        {
+            System.out.println("Largest number is: "+Num1);
+        }
+
+        else if(Num2 > Num3)
+        {
+            System.out.println("Largest number is: "+Num2);
+        }
+
+        else
+        {
+            System.out.println("Largest number is: "+Num3);
+        }
+    }
 }
