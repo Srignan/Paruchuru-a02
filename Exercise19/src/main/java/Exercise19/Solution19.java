89f19546fb501491598d435a866c5719b4ce4fc5
Srignan <Srignan7@gmail.com>
2021-09-19 11:19:06 -0400

    19 Complete

--- Exercise19/src/main/java/Exercise19/Solution19.java
+++ Exercise19/src/main/java/Exercise19/Solution19.java
@@ -6,9 +6,35 @@
 package Exercise19;
 
 /*
-Use ternary operator for print statements; if age >=16, true = old enough to drive, false = not old enough
-Scan user input for age
+Use scanner.nextDouble for input so that it only reads numbers
+Plug in formula of BMI onto program and have if else statements to have the different outputs
  */
 
+import java.util.Scanner;
+
 public class Solution19 {
+    public static void main (String[] args){
+
+        Scanner Input = new Scanner(System.in);
+
+        System.out.println("Enter the height in inches ");
+        double Height = Input.nextDouble();
+        System.out.println("Enter the weight in pounds ");
+        double Weight = Input.nextDouble();
+
+        double BMI = (Weight / (Height * Height)) * 703;
+
+        System.out.println("Your BMI is " + String.format("%.2f",BMI));
+
+
+        if(BMI >= 18.5 && BMI <= 25){
+            System.out.println("You are within the ideal weight range.");
+        }
+        else if(BMI < 18.5){
+            System.out.println("You are underweight, You should see your doctor.");
+        }
+        else if(BMI > 25){
+            System.out.println("You are overweight, You should see your doctor.");
+        }
+    }
 }
